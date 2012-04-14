efba120cf4572bf31cd7b68d6d062901d01335f1
Jason Berry <uniqueberr@gmail.com>
2012-04-14 10:55:29 -0400

    Added dependencies to the project.

---
+++ src/main/java/tmc/CrafterTracker/CrafterTrackerPlugin.java
@@ -0,0 +1,16 @@
+package tmc.CrafterTracker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CrafterTrackerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
