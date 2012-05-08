311b17fe7371e50ab13576387105bd09a37dfec7
Jason Berry <jberry@cyrusinnovation.com>
2012-05-08 18:05:04 -0400

    Added Player class and associated tests.

---
+++ src/main/scala/tmc/CrafterTracker/domain/Player.scala
@@ -0,0 +1,43 @@
+package tmc.CrafterTracker.domain
+
+import org.joda.time.DateTime
+
+// Created by cyrus on 5/8/12 at 4:48 PM
+
+class Player(name: String) {
+  var username: String = name
+  var joinedOn: DateTime = new DateTime()
+  var minutesPlayed : Long = 0
+  var blocksPlaced: Int = 0
+  var blocksBroken: Int = 0
+  var penaltyScore : Int = 0
+  var score : Int = 0
+
+  def addMinutesPlayed(minutes: Long) {
+    minutes >= 0 match {
+      case true => minutesPlayed += minutes
+      case false => throw new IllegalArgumentException("Minutes played must be greater than or equal to 0")
+    }
+  }
+
+  def addBroken(numBlocksBroken: Int) {
+    numBlocksBroken >= 0 match {
+      case true => blocksBroken += numBlocksBroken
+      case false => throw new IllegalArgumentException("Number of blocks broken must be greater than 0")
+    }
+  }
+
+  def addPlaced(numBlocksPlaced: Int) {
+    numBlocksPlaced >= 0 match {
+      case true => blocksPlaced += numBlocksPlaced
+      case false => throw new IllegalArgumentException("Number of blocks placed must be greater than 0")
+    }
+  }
+
+  def addPenaltyScore(score: Int) {
+    score >= 0 match {
+      case true => penaltyScore += score
+      case false => throw new IllegalArgumentException("Penalty score must be greater than or equal to 0")
+    }
+  }
+}
---
+++ src/test/scala/tmc/CrafterTracker/domain/PlayerTest.scala
@@ -0,0 +1,89 @@
+package tmc.CrafterTracker.domain
+
+import tmc.BukkitTestUtilities.Services.TimeFreezeService
+import org.joda.time.DateTime
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{BeforeAndAfterEach, FlatSpec}
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+// Created by cyrus on 5/8/12 at 4:43 PM
+
+@RunWith(classOf[JUnitRunner])
+class PlayerTest extends FlatSpec with ShouldMatchers with BeforeAndAfterEach {
+  var player : Player = null
+
+  override def beforeEach {
+    TimeFreezeService.freeze()
+    player = new Player("Sam")
+  }
+
+  override def afterEach {
+    TimeFreezeService.unfreeze()
+  }
+
+  it should "have a joined on date that is the date they joined the server" in {
+    player.joinedOn should equal(new DateTime())
+    player.username should equal("Sam")
+  }
+
+  it should "be able to increase the number of blocks broken" in {
+    player.blocksBroken should equal(0)
+    player.addBroken(21)
+    player.blocksBroken should equal(21)
+  }
+
+  it should "not be able to increase the number of blocks broken by a negative number" in {
+    intercept[IllegalArgumentException] {
+     player.addBroken(-21)
+    }
+  }
+
+  it should "be able to increase the number of blocks placed" in {
+     player.blocksPlaced should equal(0)
+     player.addPlaced(21)
+     player.blocksPlaced should equal(21)
+  }
+
+  it should "not be able to increase the number of blocks placed by a negative number" in {
+    intercept[IllegalArgumentException] {
+      player.addPlaced(-21)
+    }
+  }
+
+  it should "have an initial total duration of 0 minutes played" in {
+    player.minutesPlayed should equal(0)
+  }
+
+  it should "be able to increase the number of minutes played" in {
+    player.addMinutesPlayed(60)
+    player.minutesPlayed should equal(60)
+  }
+
+  it should "not be able to increase the number of minutes played by a negative value" in {
+    intercept[IllegalArgumentException] {
+      player.addMinutesPlayed(-60)
+    }
+  }
+
+  it should "initially have no penalties" in {
+    player.penaltyScore should equal(0)
+  }
+
+  it should "be able to have penalties added" in {
+    player.addPenaltyScore(3)
+    player.penaltyScore should equal(3)
+  }
+
+  it should "not be able to add a negative penalty" in {
+    intercept[IllegalArgumentException] {
+      player.addPenaltyScore(-1)
+    }
+  }
+
+  it should "have an initial score of 0" in {
+    player.score should equal(0)
+  }
+
+  ignore should "be able to calculate its score" in {}
+}
