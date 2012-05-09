2cff9a5fda8a730036ce9510494b4919138b0ebf
Jason Berry <jberry@cyrusinnovation.com>
2012-05-09 19:49:58 -0400

    Sprucing up the player chat output for the sessioninfo command.

--- src/main/scala/tmc/CrafterTracker/executors/SessionInformationExecutor.scala
+++ src/main/scala/tmc/CrafterTracker/executors/SessionInformationExecutor.scala
@@ -1,8 +1,9 @@
 package tmc.CrafterTracker.executors
 
 import org.bukkit.command.{Command, CommandSender, CommandExecutor}
-import tmc.CrafterTracker.domain.{Session, SessionMap}
 import org.bukkit.entity.Player
+import tmc.CrafterTracker.domain.{Session, SessionMap}
+import org.bukkit.ChatColor
 
 // Created by cyrus on 5/3/12 at 3:28 PM
 
@@ -10,14 +11,14 @@ class SessionInformationExecutor extends CommandExecutor {
 
   override def onCommand(sender: CommandSender, command: Command, s: String, args: Array[String]): Boolean = {
     val player: Player = sender.getServer.getPlayer(sender.getName)
-    SessionMap.applyToSessionFor(player.getName, (s:Session) => sendMessageTo(player, s))
+    SessionMap.applyToSessionFor(player.getName, (s: Session) => sendMessageTo(sender, s))
     true
   }
 
-  private def sendMessageTo(player: Player, s: Session) = {
-    player.sendMessage("Blocks broken: " + s.blocksBroken + ", " +
-      "Blocks placed: " + s.blocksPlaced + ", " +
-      "Connected at: " + s.connectedAt.toString("MM/dd/yyyy hh:mm:ss") + "," +
-      "IP address: " + s.ipAddress)
+  private def sendMessageTo(sender: CommandSender, s: Session) {
+    sender.sendMessage("You broke " + ChatColor.DARK_PURPLE + s.blocksBroken + ChatColor.WHITE + " blocks and placed " +
+                       ChatColor.DARK_PURPLE + s.blocksPlaced + ChatColor.WHITE + " blocks since you connected from " +
+                       ChatColor.DARK_PURPLE + s.ipAddress  + ChatColor.WHITE +  " at " +
+                       ChatColor.DARK_PURPLE + s.connectedAt.toString("MM/dd/yyyy hh:mm:ss") + ChatColor.WHITE + "." )
   }
 }
