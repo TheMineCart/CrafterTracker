2cf6b9140547289d999d8ce7075fcd7bb03215be
Jason Berry <jberry@cyrusinnovation.com>
2012-05-16 16:53:30 -0400

    Setting the priority for the PlayerInteractionListener to be the earliest spot in the event queue for BlockBreakEvent and BlockPlaceEvent.

--- src/main/scala/tmc/CrafterTracker/listener/PlayerInteractionListener.scala
+++ src/main/scala/tmc/CrafterTracker/listener/PlayerInteractionListener.scala
@@ -1,18 +1,18 @@
 package tmc.CrafterTracker.listener
 
-import org.bukkit.event.{EventHandler, Listener}
 import org.bukkit.event.block.{BlockPlaceEvent, BlockBreakEvent}
 import tmc.CrafterTracker.domain.{Session, SessionMap}
 import tmc.CrafterTracker.services.WarningMessageRepository
 import org.bukkit.ChatColor
 import tmc.CrafterTracker.Configuration
+import org.bukkit.event.{EventPriority, EventHandler, Listener}
 
 
 // Created by cyrus on 5/3/12 at 11:54 AM
 
 object PlayerInteractionListener extends Listener {
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
   def onBlockBreak(event: BlockBreakEvent) {
     if (hasNoUnacknowledgedWarningsFor(event.getPlayer.getName))
       SessionMap.applyToSessionFor(event.getPlayer.getName, (s: Session) => s.blockBroken)
@@ -24,7 +24,7 @@ object PlayerInteractionListener extends Listener {
     }
   }
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
   def onBlockPlace(event: BlockPlaceEvent) {
     if (hasNoUnacknowledgedWarningsFor(event.getPlayer.getName))
       SessionMap.applyToSessionFor(event.getPlayer.getName, (s: Session) => s.blockPlaced)
